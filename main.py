d650f43ce985013d70f909aec949cb36e401ce33
Wozniak19 <100923686+Wozniak19@users.noreply.github.com>
2024-08-28 13:33:43 +0000

    Update main.py
    Added proba as one endpoint

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ async def read_root(input:dict):
     df = pd.DataFrame(input,index=range(1))
 
     predicted =  model.predict(df.values)
+    
     Depression = str(predicted[0][0])
     Schizophrenia = str(predicted[0][1])
     Acute_and_transient_psychotic_disorder = str(predicted[0][2])
@@ -35,27 +36,69 @@ async def read_root(input:dict):
     Gambling = str(predicted[0][12])
     substance_abuse = str(predicted[0][13])
     Others = str(predicted[0][14])
+
+    proba = model.predict_proba(df.values)
+    Depression_proba = str(proba[0][0])
+    Schizophrenia_proba = str(proba[0][1])
+    Acute_and_transient_psychotic_disorder_proba = str(proba[0][2])
+    Delusional_Disorder_proba = str(proba[0][3])
+    BiPolar1_proba = str(proba[0][4])
+    BiPolar2_proba = str(proba[0][5])
+    Generalized_Anxiety_proba = str(proba[0][6])
+    Panic_Disorder_proba = str(proba[0][7])
+    Specific_Phobia_proba = str(proba[0][8])
+    Social_Anxiety_proba = str(proba[0][9])
+    OCD_proba = str(proba[0][10])
+    PTSD_proba = str(proba[0][11])
+    Gambling_proba = str(proba[0][12])
+    substance_abuse_proba = str(proba[0][13])
+    Others_proba = str(proba[0][14])
+    
     
 
 
     return {
-        "Depression":Depression,
-        "Schizophrenia":Schizophrenia,
-        "Delusional_Disorder":Delusional_Disorder,
-        "BiPolar1":BiPolar1,
-        "BiPolar2":BiPolar2,
-        "Generalized_Anxiety":Generalized_Anxiety,
-        "Panic_Disorder":Panic_Disorder,
-        "Specific_Phobia":Specific_Phobia,
-        "Social_Anxiety":Social_Anxiety,
-        "OCD":OCD,
-        "PTSD": PTSD,
-        "Gambling":Gambling,
-        "substance_abuse":substance_abuse,
-        "Others":Others
+        "Disorder": {
+            
+            "Depression":Depression,
+            "Schizophrenia":Schizophrenia,
+            "Delusional_Disorder":Delusional_Disorder,
+            "BiPolar1":BiPolar1,
+            "BiPolar2":BiPolar2,
+            "Generalized_Anxiety":Generalized_Anxiety,
+            "Panic_Disorder":Panic_Disorder,
+            "Specific_Phobia":Specific_Phobia,
+            "Social_Anxiety":Social_Anxiety,
+            "OCD":OCD,
+            "PTSD": PTSD,
+            "Gambling":Gambling,
+            "substance_abuse":substance_abuse,
+            "Others":Others
+        },
+        "Probabilities":{
+
+            "Depression_proba":Depression_proba,
+            "Schizophrenia_proba":Schizophrenia_proba,
+            "Acute_and_transient_psychotic_disorder_proba":Acute_and_transient_psychotic_disorder_proba,
+            "Delusional_Disorder_proba":Delusional_Disorder_proba,
+            "BiPolar1_proba":BiPolar1_proba,  
+            "BiPolar2_proba":BiPolar2_proba,
+            "Generalized_Anxiety_proba":Generalized_Anxiety_proba,
+            "Panic_Disorder_proba":Panic_Disorder_proba,
+            "Specific_Phobia_proba":Specific_Phobia_proba, 
+            "Social_Anxiety_proba":Social_Anxiety_proba,
+            "OCD_proba":OCD_proba, 
+            "PTSD_proba":PTSD_proba, 
+            "Gambling_proba":Gambling_proba, 
+            "substance_abuse_proba":substance_abuse_proba,
+            "Others_proba":Others_proba,
+
+
+        
+        }
 
     }
-    
+ '''   
 @app.post("/predict_proba")
 async def predict_proba(input: dict):
     df = pd.DataFrame(input, index=range(1))
@@ -86,7 +129,7 @@ async def predict_proba(input: dict):
         "Probabilities": disorder_probabilities
     }
 
-
+'''
 
 
 
